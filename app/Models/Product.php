3919cbe2ea25a5d660d0b5ae1126356bd1641ac3
MoEmad2000 <mohamedemad0592@gmail.com>
2026-06-30 21:19:17 +0300

    install api

---
+++ app/Models/Product.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    use HasUuids, SoftDeletes;
+
+    protected $fillable = [
+        'sku',
+        'name',
+        'description',
+        'price',
+        'stock_quantity',
+        'low_stock_threshold',
+        'status',
+    ];
+}
